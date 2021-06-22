e546b1b1999783e84ce6b65e2f5fe795fdb7faab
Loc Do <lockunlatui@gmail.com>
2021-06-22 21:47:28 +0700

    apply css for page not found

--- src/containers/NotFound/index.tsx
+++ src/containers/NotFound/index.tsx
@@ -1,5 +1,32 @@
+import { Typography, Button } from "@components/index";
+import Styles from "./notFound.module.scss";
 const NotFound = () => {
-  return <section>This is not the page that you are looking for!</section>;
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.notFoundContainer}>
+        <div className={Styles.notFoundContent}>
+          <Typography variant="h5">Trang này không còn tồn tại.</Typography>
+          <Typography variant="body2">
+            We couldn’t find the page you were looking for. Maybe our FAQ or
+            Community can help?
+          </Typography>
+          <Button>Quay Lại</Button>
+        </div>
+        <div className={Styles.notFoundImage}>
+          <img
+            alt="Record"
+            className="img-responsive record"
+            src="https://www.scdn.co/i/404/record.svg"
+          />
+          <img
+            className={Styles.recordArm}
+            alt="RecordArm"
+            src="https://www.scdn.co/i/404/record-arm.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
