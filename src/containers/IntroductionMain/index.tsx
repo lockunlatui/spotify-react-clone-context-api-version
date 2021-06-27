efc67cd1fe1b64dfc97ea5d192bfde8256037b34
Loc Do <lockunlatui@gmail.com>
2021-06-27 16:35:46 +0700

    apply make the most section

--- src/containers/IntroductionMain/index.tsx
+++ src/containers/IntroductionMain/index.tsx
@@ -1,9 +1,11 @@
 import LookingForMusic from "./LookingForMusic";
+import MakeTheMost from "./MakeTheMost";
 
 const IntroductionMain = () => {
   return (
     <section>
       <LookingForMusic />
+      <MakeTheMost />
     </section>
   );
 };
